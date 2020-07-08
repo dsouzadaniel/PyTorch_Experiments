e0bd0a833edf89ee9bdecb95c28813789b0df34a
danieldsouza <ddsouza@umich.edu>
2020-07-07 20:34:03 -0400

    # Added Epochwise Accuracy

--- lightning/with_lightning.py
+++ lightning/with_lightning.py
@@ -10,7 +10,7 @@ from torchvision.datasets import MNIST
 from torch.utils.data.dataset import random_split
 
 from pytorch_lightning.core.lightning import LightningModule
-
+from pytorch_lightning.metrics import Accuracy
 
 class LitModel(LightningModule):
 
@@ -18,10 +18,15 @@ class LitModel(LightningModule):
         super().__init__()
         self.layer1 = torch.nn.Linear(28 * 28, 128)
         self.layer2 = torch.nn.Linear(128, 10)
+        self.metric_accu = Accuracy(num_classes=10)
 
     def forward(self, x):
         x = x.view(x.size(0), -1)
         x = self.layer1(x)
+        # Randomness
+        # random_ixs = torch.randperm(n=len(x))
+        # x = x[random_ixs]
+
         x = F.relu(x)
         x = self.layer2(x)
         return x
@@ -30,22 +35,43 @@ class LitModel(LightningModule):
         x, y = batch
         y_hat = self.forward(x)
         loss = F.cross_entropy(y_hat, y)
-        return {'loss': loss}
+        y_hat_argmax = y_hat.argmax(dim=1)
+        pred = y_hat.argmax(dim=1)
+        return {'loss': loss, 'true':y, 'pred':pred}
 
     def training_epoch_end(self, outputs):
         avg_loss = torch.stack([x['loss'] for x in outputs]).mean()
-        tensorboard_logs = {'loss/train': avg_loss,  'step': self.current_epoch}
+
+        all_true = torch.cat([x['true'] for x in outputs], dim=0).squeeze()
+        all_pred = torch.cat([x['pred'] for x in outputs], dim=0).squeeze()
+        #
+        # print("X_true->{0}".format(all_true.shape))
+        # print("Y_true->{0}".format(all_pred.shape))
+
+        accuracy = self.metric_accu(all_pred, all_true)
+
+        tensorboard_logs = {'loss/train': avg_loss, 'accu/train': accuracy, 'step': self.current_epoch}
         return {'loss/train': avg_loss, 'log': tensorboard_logs}
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self.forward(x)
         loss = F.cross_entropy(y_hat, y)
-        return {'loss': loss}
+        pred = y_hat.argmax(dim=1)
+        return {'loss': loss, 'true':y, 'pred':pred}
 
     def validation_epoch_end(self, outputs):
         avg_loss = torch.stack([x['loss'] for x in outputs]).mean()
-        tensorboard_logs = {'loss/val': avg_loss,  'step': self.current_epoch}
+
+        all_true = torch.cat([x['true'] for x in outputs], dim=0).squeeze()
+        all_pred = torch.cat([x['pred'] for x in outputs], dim=0).squeeze()
+
+        # print("X_val->{0}".format(all_true.shape))
+        # print("Y_val->{0}".format(all_pred.shape))
+
+        accuracy = self.metric_accu(all_pred, all_true)
+
+        tensorboard_logs = {'loss/val': avg_loss,'accu/val': accuracy, 'step': self.current_epoch}
         return {'loss/val': avg_loss, 'log': tensorboard_logs}
 
     def configure_optimizers(self):
@@ -63,11 +89,10 @@ class LitModel(LightningModule):
         loader = DataLoader(mnist_test, batch_size=64, num_workers=4, shuffle=False)
         return loader
 
-
 from pytorch_lightning import Trainer
 
 model = LitModel()
 
-trainer = Trainer(max_epochs=5)
+trainer = Trainer(max_epochs=10)
 
 trainer.fit(model)
